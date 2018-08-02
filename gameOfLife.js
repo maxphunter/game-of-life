f4d187694339d2c555c58f94af828dd420e645d4
Max Hunter <max_phunter@yahoo.com>
2018-08-02 13:04:35 -0400

    initial commit for iteration logic

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,4 +1,44 @@
-function gameOfLife(a, b) {
-  return a + b;
+function gameOfLife(initialState) {
+  
+  var updatedState = []
+
+  //iterate over rows
+  for(var y = 0; y < initialState.length; y++){
+
+    //iterate over columns within each row
+      for(var x = 0; x < initialState[y].length; x++){
+
+        //check all the surroundings
+        //check left
+        let leftCell = initialState[x-1][y]
+        //check upper left
+        let upperLeftCell = initialState[x-1][y-1]
+        //check above
+        let aboveCell = initialState[x][y-1]
+        //check upper right
+        let upperRightCell = initialState[x+1][y-1]
+        //check right
+        let rightCell = initialState[x+1][y]
+        //check lower right
+        let lowerRightCell = initialState[x+1][y+1]
+        //check below
+        let belowCell= initialState[x][y]
+        //check lower left
+        let lowerLeftCell = initialState[x-1][y+1]
+
+        //count total alive
+        //count total dead
+
+        //determine new state based on alive/dead neighbor count
+
+        //write new state to updated array
+
+
+      }
+
+
+  }
+
+
 }
 module.exports = gameOfLife;
--- gameOfLife.test.js
+++ gameOfLife.test.js
@@ -1,5 +1,25 @@
 const gameOfLife = require('./gameOfLife');
 
+let alive = true;
+let dead = false;
+
+
+
 test('adds 1 + 2 to equal 3', () => {
-  expect(gameOfLife(1, 2)).toBe(3);
+
+  var initialState = [[dead , dead , dead , dead , dead , dead , alive, dead],
+                      [alive, alive, alive, dead , dead , dead , alive, dead],
+                      [dead , dead , dead , dead , dead , dead , alive, dead], 
+                      [dead , dead , dead , dead , dead , dead , dead , dead],
+                      [dead , dead , dead , alive, alive, dead , dead , dead],
+                      [dead , dead , dead , alive, alive, dead , dead , dead]];
+
+  let expectedState =  [[dead , alive, dead , dead , dead , dead , dead , dead],
+                        [dead , alive, dead , dead , dead , alive, alive, alive],
+                        [dead , alive, dead , dead , dead , dead , dead , dead], 
+                        [dead , dead , dead , dead , dead , dead , dead , dead],
+                        [dead , dead , dead , alive, alive, dead , dead , dead],
+                        [dead , dead , dead , alive, alive, dead , dead , dead]];
+
+  expect(gameOfLife(initialState)).toBe(expectedState);
 });
